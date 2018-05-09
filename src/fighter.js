9de6ec46c25cec965ec116038c8704e893d08c44
Danil Davydov <Linadakill@gmail.com>
2018-05-09 13:34:40 +0300

    create class Fighter

--- src/fighter.js
+++ src/fighter.js
@@ -1,1 +1,32 @@
 // Create class Fighter
+
+class Fighter{
+
+    constructor(name, health, power){
+        this.name = name
+        this.health = health
+        this.power = power
+    }
+
+    setDamage(demage){
+        console.log( this.health -= demage )
+    }
+
+    hit(enemy, point){
+        let damage = point * this.power
+        enemy.setDamage(damage)        
+    }
+
+    knockout(){
+        return new Promise (function(resolve, reject){
+
+            setTimeout( () => {
+                console.log('time is ower')
+                resolve()
+            }, 500 )
+
+        })
+    }
+}
+
+

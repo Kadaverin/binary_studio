7c83cd6341ff9c03d80c9bf4334cc7458f78fbe6
Danil Davydov <Linadakill@gmail.com>
2018-05-09 15:48:25 +0300

    create fight function

--- src/fight.js
+++ src/fight.js
@@ -11,17 +11,20 @@ async function fight(fighter, improvedFighter , ...points){
             fighter.hit(improvedFighter,  points[index])
         }
 
-        if (fighter.health < 0){
+        if (fighter.health <= 0){
             await fighter.knockout()
             console.log (`${fighter.name} is dead`)
             break
         }
 
-        if (improvedFighter.health < 0) {
+        if (improvedFighter.health <= 0) {
             await improvedFighter.knockout()
             console.log (`${improvedFighter.name} is dead`)
             break
         }
-
+    }
+    
+    if ( (fighter.health > 0) && (ImprovedFighter.health > 0) ) {
+        console.log('This round ended in a draw')
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "babel-polyfill";
 
 // create two instances
+fighter = new Fighter ('John' , 100 , 3)
+improvedFighter = new ImprovedFighter('Smith', 100 ,2)
 
-// call fight function
+// create random points
+ points = Array.from({length: 25}, () => Math.floor(Math.random() * 40));
+ 
+ // call fight function
+ fight(fighter, improvedFighter, ...points)

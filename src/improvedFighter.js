660eaec92c4855c697d08efd491dbc6606a1cd06
Danil Davydov <Linadakill@gmail.com>
2018-05-09 13:35:18 +0300

    create class ImprovedFighter

--- src/improvedFighter.js
+++ src/improvedFighter.js
@@ -1,1 +1,14 @@
 // Create class ImprovedFighter
+
+class ImprovedFighter extends Fighter{
+    
+    constructor(name, health, power){
+        super(name, health, power)
+    }
+
+    doubleHit(enemy, point){
+        let damage = 2 * point * this.power
+        enemy.setDamage( damage )
+    }
+
+}

e1b36ec61b8754acccbb1410495087efd86edc16
Danil Davydov <Linadakill@gmail.com>
2018-05-09 14:33:51 +0300

    create fight function

--- src/fight.js
+++ src/fight.js
@@ -1,1 +1,27 @@
 // create async function fight
+
+async function fight(fighter, improvedFighter , ...points){
+
+    for ( index in  points ) {
+
+        if (index % 2 == 0){
+            improvedFighter.doubleHit(fighter , points[index])
+        }
+        else {
+            fighter.hit(improvedFighter,  points[index])
+        }
+
+        if (fighter.health < 0){
+            await fighter.knockout()
+            console.log (`${fighter.name} is dead`)
+            break
+        }
+
+        if (improvedFighter.health < 0) {
+            await improvedFighter.knockout()
+            console.log (`${improvedFighter.name} is dead`)
+            break
+        }
+
+    }
+}
